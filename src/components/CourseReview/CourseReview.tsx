1363fecc37b4f330e6d7627421fc8f2a4d9a76fc
Ian D'Souza <dsouza.ianp@gmail.com>
2023-11-12 03:57:31 -0500

    implement comments

--- src/components/CourseReview/CourseReview.tsx
+++ src/components/CourseReview/CourseReview.tsx
@@ -1,82 +1,100 @@
-import {
-    Badge,
-    Group,
-    Title,
-    Text,
-    Card,
-    SimpleGrid,
-    Container,
-    rem,
-    RingProgress
-} from '@mantine/core';
+import { Text, Card, SimpleGrid, Container, RingProgress } from '@mantine/core';
 import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
 import classes from './CourseReview.module.css';
+import { Comment } from '@/components/Comment/Comment';
 
-const mockdata = [
+const comments = [
     {
-        title: 'Avgerage rating',
-        value: 9,
-        isDifficulty: false,
+      rating: 8,
+      difficulty: 6,
+      professor: 'John Doe',
+      description: 'This course was great! The professor was very knowledgeable and engaging.',
     },
     {
-        title: 'Average difficulty',
-        value: 7,
-        isDifficulty: true,
+      rating: 7,
+      difficulty: 8,
+      professor: 'Jane Smith',
+      description: 'This course was challenging but rewarding. The professor was very helpful and supportive.',
     },
-    {
-        title: 'No third parties',
-        value: 3,
-        isDifficulty: false,
-    },
-];
-
-export function CourseReview({ courseName }) {
+  ];
+  
+  export function CourseReview({ courseName }) {
     const getRatingColor = (rating, isDifficulty = false) => {
-        if (isDifficulty) {
-            if (rating <= 4) return 'green';
-            if (rating <= 7.5) return 'orange';
-            return 'red';
-        } else {
-            if (rating >= 7.5) return 'green';
-            if (rating >= 4) return 'orange';
-            return 'red';
-        }
+      if (isDifficulty) {
+        if (rating <= 4) return 'green';
+        if (rating <= 7.5) return 'orange';
+        return 'red';
+      } else {
+        if (rating >= 7.5) return 'green';
+        if (rating >= 4) return 'orange';
+        return 'red';
+      }
     };
-    const features = mockdata.map((feature) => (
-        <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
-            <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
-                {feature.title}
-            </Text>
+  
+    const calculateAverage = (property) => {
+      const sum = comments.reduce((acc, comment) => acc + comment[property], 0);
+      return sum / comments.length;
+    };
+  
+    const averageRating = calculateAverage('rating');
+    const averageDifficulty = calculateAverage('difficulty');
+  
+    const features = [
+      {
+        title: 'Average rating',
+        value: averageRating,
+        isDifficulty: false,
+      },
+      {
+        title: 'Average difficulty',
+        value: averageDifficulty,
+        isDifficulty: true,
+      },
+      {
+        title: 'Number of reviews',
+        value: comments.length,
+        isDifficulty: false,
+      },
+    ].map((feature) => (
+      <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
+        <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+          {feature.title}
+        </Text>
+        {feature.title === 'Number of reviews' ? (
+          <Text fz="xl" fw={700} ta="center" className={classes.cardValue}>
+            {feature.value}
+          </Text>
+        ) : (
+          <div style={{ width: '100px', height: '100px' }}>
             <RingProgress
-                sections={[{ value: feature.value*10, color: getRatingColor(feature.value) }]}
-                label={
-                    <Text c={getRatingColor(feature.value)} fw={700} ta="center" size="xl">
-                        {feature.value}/10
-                    </Text>
-                }
+              sections={[{ value: feature.value * 10, color: getRatingColor(feature.value, feature.isDifficulty) }]}
+              label={
+                <Text c={getRatingColor(feature.value, feature.isDifficulty)} fw={700} ta="center" size="xl">
+                  {feature.value.toFixed(1)}/10
+                </Text>
+              }
             />
-        </Card>
+          </div>
+        )}
+      </Card>
     ));
-
+  
+    const commentCards = comments.map((comment, index) => (
+      <Comment key={index} rating={comment.rating} difficulty={comment.difficulty} professor={comment.professor} description={comment.description} />
+    ));
+  
     return (
-        <Container size="lg" py="xl">
-            {/* <Group justify="center">
-          <Badge variant="filled" size="lg">
-            Best company ever
-          </Badge>
-        </Group> */}
-
-            <Title order={2} className={classes.title} ta="center" mt="sm">
-                {courseName}
-            </Title>
-
-            <Text c="dimmed" className={classes.description} ta="center" mt="md">
-                Course Description
-            </Text>
-
-            <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
-                {features}
-            </SimpleGrid>
-        </Container>
+      <Container size="lg" py="xl">
+        <Text size="xl" mb="lg">
+          {courseName}
+        </Text>
+        <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+          {features}
+        </SimpleGrid>
+        <Text size="lg" mt="xl">
+          Comments
+        </Text>
+        {commentCards}
+      </Container>
     );
-}
+  }
