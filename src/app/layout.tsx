d9adeb7a985f53baea780b60c71a12e5e7ec56e3
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-11-12 01:57:12 -0500

    Revert "auth0 config"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../../theme";
 import { HeaderMenu } from "@/components/HeaderMenu/HeaderMenu";
-import {UserProvider} from "@auth0/nextjs-auth0/client"
 
 export const metadata = {
   title: "Mantine Next.js template",
@@ -12,23 +11,21 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-      <html lang="en">
-         <UserProvider>
-        <head>
-          <ColorSchemeScript />
-          <link rel="shortcut icon" href="/favicon.svg" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-          />
-        </head>
-        <body>
-          <MantineProvider theme={theme}>
-            <HeaderMenu />
-            {children}
-          </MantineProvider>
-        </body>
-        </UserProvider>
-      </html>      
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>
+          <HeaderMenu />
+          {children}
+        </MantineProvider>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Hero } from '@/components/Hero/Hero';
-import Login from './login';
+
 export default function HomePage() {
   return (
-    <div>
-      <Login></Login>
-      <Hero />
-    </div>
+    <>
+    <Hero />
+    </>
   );
 }
